f176ae419c11fe615ed18b665ab509211db670b4
Matheus Alano Joenck <matheusalanojoenck@gmail.com>
2024-07-04 15:51:21 -0300

    Aula 4.2: Melhorando nossa tipagem, Repositories e o começo da paginação (parte 2)

--- src/ui/repository/todo.ts
+++ src/ui/repository/todo.ts
@@ -14,7 +14,9 @@ function get({
 }: TodoRepositoryGetParams): Promise<TodoRepositoryGetOutput> {
     return fetch("/api/todos").then(async (todosResponse) => {
         const todosServerString = await todosResponse.text();
-        const todosServer = JSON.parse(todosServerString).todos;
+        const todosServer = parseTodosFromServer(
+            JSON.parse(todosServerString)
+        ).todos;
 
         const ALL_TODOS = todosServer;
         const startIndex = (page - 1) * limit;
@@ -39,4 +41,39 @@ interface Todo {
     content: string;
     date: Date;
     done: boolean;
+}
+
+function parseTodosFromServer(responseBody: unknown): { todos: Array<Todo> } {
+    if (
+        responseBody !== null &&
+        typeof responseBody === "object" &&
+        "todos" in responseBody &&
+        Array.isArray(responseBody.todos)
+    ) {
+        return {
+            todos: responseBody.todos.map((todo: unknown) => {
+                if (todo === null && typeof todo !== "object") {
+                    throw new Error("Invalid todo from API");
+                }
+
+                const { id, content, done, date } = todo as {
+                    id: string;
+                    content: string;
+                    date: string;
+                    done: string;
+                };
+
+                return {
+                    id,
+                    content,
+                    done: String(done).toLowerCase() === "true",
+                    date: new Date(date),
+                };
+            }),
+        };
+    }
+
+    return {
+        todos: [],
+    };
 }
