d93dad080e52a6cd18b534dc9d805693a543544f
Matheus Alano Joenck <matheusalanojoenck@gmail.com>
2024-07-12 15:12:14 -0300

    A6.2: GitHub Actions e automatizando nossos testes e o linter - Parte 1

--- core/crud.ts
+++ core/crud.ts
@@ -78,7 +78,7 @@ export function update(id: UUID, partialTodo: Partial<Todo>): Todo {
     return updatedTodo;
 }
 
-function updateContentById(id: UUID, content: string): Todo {
+export function updateContentById(id: UUID, content: string): Todo {
     return update(id, {
         content,
     });
@@ -107,12 +107,12 @@ export function deleteById(id: UUID) {
     );
 }
 
-function CLEAR_DB() {
+export function CLEAR_DB() {
     fs.writeFileSync(DB_FILE_PATH, "");
 }
 
 // [SIMULATION]
-CLEAR_DB();
+/* CLEAR_DB();
 create("Primeira TODO");
 const secondTodo = create("Segunda TODO");
 deleteById(secondTodo.id);
@@ -127,4 +127,4 @@ create("Limpar a casa");
 //   done: true,
 // });
 updateContentById(thirdTodo.id, "Atualizada!");
-//console.log(read());
+//console.log(read()); */
--- cypress.config.ts
+++ cypress.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
     e2e: {
         video: false,
         screenshotOnRunFailure: false,
-        setupNodeEvents(on, config) {
+        setupNodeEvents() {
             // implement node event listeners here
         },
     },
