5ebbb1166d2804317f35498cc5c0a98d28e2b8a8
Matheus Alano Joenck <matheusalanojoenck@gmail.com>
2024-07-15 19:09:43 -0300

    fix favicon.ico get error 404

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,6 +13,9 @@ export default function RootLayout({
 }) {
     return (
         <html lang="en">
+            <head>
+                <link rel="shortcut icon" href="#"></link>
+            </head>
             <body>
                 <StyledJsxRegistry>{children}</StyledJsxRegistry>
             </body>
