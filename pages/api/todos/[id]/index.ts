6444b699b82d6fd80f9e959d0d7ddb5833edb2b6
Matheus Alano Joenck <matheusalanojoenck@gmail.com>
2024-07-15 15:17:27 -0300

    update

---
+++ pages/api/todos/[id]/index.ts
@@ -0,0 +1,18 @@
+import { todoController } from "@server/controller/todo";
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(
+    request: NextApiRequest,
+    response: NextApiResponse
+) {
+    if (request.method === "DELETE") {
+        await todoController.deleteById(request, response);
+        return;
+    }
+
+    response.status(405).json({
+        error: {
+            message: "Method not allowed",
+        },
+    });
+}
